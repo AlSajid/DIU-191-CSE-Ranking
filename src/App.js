bf409d8a8026273f80d6c7ad89925e296d2a7f91
Md. Al-Sajiduzzaman Akand <alsajiduzzaman@outlook.com>
2023-01-13 22:15:42 +0600

    search added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import JsonQuery from 'json-query';
 import { useEffect, useState } from 'react';
-import ranking from './ranking.json';
+// import ranking from './ranking.json';
 
 function App() {
-  const students = [];
-  let rank = 1;
+  const [students, setStudents] = useState([]);
+  const [storage, setStorage] = useState([]);
 
-  for (let i = 0; i < ranking.length; i++) {
-    if (i === 0) {
-      students.push({ ...ranking[i], rank });
+  useEffect(() => {
+    fetch('ranking.json')
+      .then((res) => res.json())
+      .then((data) => {
+        setStudents(data)
+        setStorage(data)
+      });
+  }, []);
 
+
+
+  const handleSearch = (searchValue) => {
+    const id = JsonQuery(`[*][*studentId~/${searchValue}/i]`,
+      { data: students, allowRegexp: true }).value;
+
+    const name = JsonQuery(`[*][*studentName~/${searchValue}/i]`,
+      { data: students, allowRegexp: true }).value;
+
+    if (id.length > name.length) {
+      setStudents(id);
+    } else if (id.length < name.length) {
+      setStudents(name);
     } else {
-      if (ranking[i].cgpa === ranking[i - 1].cgpa) {
-        students.push({ ...ranking[i], rank });
-      } else {
-        rank = rank + 1;
-        students.push({ ...ranking[i], rank });
-      }
+      setStudents(storage);
     }
-  }
 
+    console.log(id.length, name.length)
+  }
 
 
   return (
@@ -29,30 +42,43 @@ function App() {
         <h1>Report/Feedback</h1>
       </a>
 
-      <table className='table-auto w-11/12 mx-auto'>
-        <tr className='font-bold text-xl '>
-          <td className="w-1">No</td>
-          <td className='p-3'>Rank</td>
-          <td>Name</td>
-          <td>Campus</td>
-          <td>Student ID</td>
-          <td>CGPA</td>
-        </tr>
 
-        {students?.length === 0 && <tr><td className='text-center font-xxl'>Loading...</td></tr>}
-        {students?.map((item, serial) => {
-          return (
-            <tr className='border text-lg' key={item.studentId}>
-              <td className='text-center text-slate-500'>{serial+1}</td>
-              <td className='text-center'>{item.rank}</td>
-              <td className='p-3'>{item.studentName}</td>
-              <td className='p-3'>{item.campusName}</td>
-              <td>{item.studentId}</td>
-              <td>{item?.cgpa}</td>
-            </tr>
-          );
-        })}
+      <table className='table-auto w-11/12 mx-auto'>
+        <tbody>
+          <tr>
+            <td colSpan={6}>
+              <input onChange={(e) => handleSearch(e.target.value)} type="text" className='w-full p-3 text-center border rounded-lg' placeholder="191-15-2523 or Abdur Rahman" />
+
+            </td>
+          </tr>
+
+          <tr className='font-bold text-xl '>
+            <td className="w-1">No</td>
+            <td className='p-3'>Rank</td>
+            <td>Name</td>
+            <td>Campus</td>
+            <td>Student ID</td>
+            <td>CGPA</td>
+          </tr>
+
+
+          {students?.length === 0 && <tr><td className='text-center font-xxl'>Loading...</td></tr>}
+
+          {students?.map((item, serial) => {
+            return (
+              <tr className='border text-lg' key={item.studentId}>
+                <td className='text-center text-slate-500'>{serial + 1}</td>
+                <td className='text-center'>{item.rank}</td>
+                <td className='p-3'>{item.studentName}</td>
+                <td className='p-3'>{item.campusName}</td>
+                <td>{item.studentId}</td>
+                <td>{item?.cgpa}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
+
     </div>
   );
 }
